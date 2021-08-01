9a91653b33c8d3686bf16ea1d318efb6c99dd31b
YashKumarSuman <yash.suman.e21@nsut.ac.in>
2021-08-01 15:19:00 +0530

    updated

--- wcat.js
+++ wcat.js
@@ -25,11 +25,11 @@ if(filesArr.length==0)
     return;
 }
 //******************* check for both input simultaneously************
-if(optionsArr.includes("-n") && optionsArr.includes("-b")==true)
-{
-    console.log(`-b and -n can't be executed at the same time. Please enter only one at a time`);
-    return;
-}
+// if(optionsArr.includes("-n") && optionsArr.includes("-b")==true)
+// {
+//     console.log(`-b and -n can't be executed at the same time. Please enter only one at a time`);
+//     return;
+// }
 
 //******************* check for file existence*********************
 for(i=0;i<filesArr.length;i++)
@@ -81,9 +81,43 @@ if(optionsArr.includes("-s")==true)
     console.log("-------- (-s command result)--------")
     console.log(contentArr.join("\n"));
 }
+let indexofN = optionsArr.indexOf("-n");
+let indexofB = optionsArr.indexOf("-b");
+let finaloption = "";
+if(indexofN>-1 && indexofB>-1)
+{
+    if(indexofN<indexofB)
+    {
+        finaloption="-n";
+    }
+    else{
+        finaloption="-b";
+    }
+}
+else{
+    if(indexofB>-1)
+    {
+        finaloption="-b";
+    }
+    else if(indexofN>-1)
+    {
+        finaloption="-n"
+    }
+}
+if(finaloption=="-n")
+{
+    modifyN(contentArr);
+}
+else if(finaloption=="-b")
+{
+    modifyB(contentArr);
+}
+
 
 //************************If -n is present ********************
-if(optionsArr.includes("-n")==true)
+//if(optionsArr.includes("-n")==true)
+function modifyN(contentArr)
+{
 {
   for(i=0;i<contentArr.length;i++)
   {
@@ -92,9 +126,11 @@ if(optionsArr.includes("-n")==true)
   console.log("-------- (-n command result)--------");
 console.log(contentArr.join("\n"));
 }
-
+}
 //************************If -b is present ********************
-if(optionsArr.includes("-b")==true)
+function modifyB(contentArr)
+{
+//if(optionsArr.includes("-b")==true)
 {
     let c=1;
     for(i=0;i<contentArr.length;i++)
@@ -107,5 +143,6 @@ if(optionsArr.includes("-b")==true)
   }
   console.log("---- (-b command result) ---------");
   console.log(contentArr.join("\n"));
+}
 }
 
